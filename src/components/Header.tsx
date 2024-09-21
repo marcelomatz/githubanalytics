93e32f28b3d93e5eda060ee573b4f6e57eee9f16
marcelomatz <omarcelomatz@gmail.com>
2024-09-20 21:31:42 -0300

    ajustando title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,18 @@ import { Separator } from "./ui/separator";
 import Link from "next/link";
 
 const Header = () => {
- 
   return (
     <div className="w-full justify-center bg-zinc-900 bg-background dark:bg-foreground py-8">
       <div className="flex w-full max-w-7xl px-4 lg:px-0 mx-auto justify-between">
-      <h1 className="text-2xl bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600"><Link href={"/"}>GitHub <span className="font-bold">Explorer</span></Link></h1>
-      
+        <h1 className="text-2xl bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
+          <Link href={"/"}>
+            GitHub | <span className="font-bold">Explorer</span>
+          </Link>
+        </h1>
       </div>
       <Separator className="my-4 dark:bg-foreground" />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
